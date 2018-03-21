e1799a365c5aa9c5d6fae827f98942df5829c7a9
Clara <claralangen@hotmail.fr>
2018-03-21 20:04:02 +0100

    database + structure

---
+++ src/js/data.js
@@ -0,0 +1,37 @@
+var composers = [
+    {
+        composerName :  'John Williams',
+        facePicture : 'imgs/face/JW.png',
+        coverPicture : 'imgs/cover/JW.png',
+        movies: [{
+            name : 'La_Guerre_des_Étoiles',
+            picture : 'imgs/movie/La_Guerre_des_Étoiles.png',
+            date : '1977',
+            biography:'hello',
+            musics : [
+                {
+                    name : 'Rebel Blockade Runner',
+                    id : '1T5_82U3JVo',
+                    duration : 300
+                },
+               {
+                    name : 'Imperial Attack',
+                    id : 'j1pf6s8MGrs',
+                    duration : 300
+                },
+                {
+                    name : 'The Dune Sea of Tatooine',
+                    id : 'ZxMdImpj4mk',
+                    duration : 300
+                },
+                {
+                    name : 'The Moisture Farm',
+                    id : '7WCUPbHunNo',
+                    duration : 300
+                }
+            ]
+        }
+        ]
+    }
+];
+
