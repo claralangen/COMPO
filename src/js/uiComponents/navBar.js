224ed4165ad8520e4ec004d1959caaab668ac538
Draktors <enzo.hespel@hotmail.fr>
2018-04-07 20:09:06 +0200

    nav fixed + display poster/compo

--- src/js/uiComponents/navBar.js
+++ src/js/uiComponents/navBar.js
@@ -1,7 +1,10 @@
 const recIntoCircle = document.querySelector('.navRec');
 const recs = document.querySelectorAll('.navRec-item');
+const posterContainer = document.querySelector('.posterContainer');
 
 recIntoCircle.addEventListener('click',function () {
+    posterContainer.classList.toggle('is-hidden');
+    console.log(posterContainer)
     for (let i = 0; i < recs.length; i++) {
         recs[i].classList.toggle('navRecRadius');
     }
